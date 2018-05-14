eb36d6728f39fe433580012c27cc16bbafb3f71b
P3tr0viCh <P3tr0viCh@mail.ru>
2018-05-14 14:01:48 +0500

    Добавлена библиотека шифрования Crypto++
    Версия 7.0

---
+++ UtilsCryptoPP.cpp
@@ -0,0 +1,41 @@
+// ---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "UtilsCryptoPP.h"
+// ---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+#include "default.h"
+#include "hex.h"
+#include "aes.h"
+
+// ---------------------------------------------------------------------------
+String EncryptAES(String Text, String Key) {
+	CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption e((byte*)Key.c_str(),
+		Key.Length());
+
+	std::string Result;
+
+	CryptoPP::StringSource encryptor(AnsiString(Text).c_str(), true,
+		new CryptoPP::StreamTransformationFilter(e,
+		new CryptoPP::HexEncoder(new CryptoPP::StringSink(Result))));
+
+	return String(Result.c_str());
+}
+
+// ---------------------------------------------------------------------------
+String DecryptAES(String Text, String Key) {
+	CryptoPP::ECB_Mode<CryptoPP::AES>::Decryption d((byte*)Key.c_str(),
+		Key.Length());
+
+	std::string Result;
+
+	CryptoPP::StringSource decryptor(AnsiString(Text).c_str(), true,
+		new CryptoPP::HexDecoder(new CryptoPP::StreamTransformationFilter(d,
+		new CryptoPP::StringSink(Result))));
+
+	return String(Result.c_str());
+}
+
+// ---------------------------------------------------------------------------
---
+++ UtilsCryptoPP.h
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------------------
+
+#ifndef UtilsCryptoPPH
+#define UtilsCryptoPPH
+
+#include <System.hpp>
+
+// ---------------------------------------------------------------------------
+String EncryptAES(String Text, String Key);
+String DecryptAES(String Text, String Key);
+
+// ---------------------------------------------------------------------------
+#endif
