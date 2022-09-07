86dfb333d5f6e1bac804ba3181b8d03058f145c2
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2022-09-07 16:32:22 +0500

    CodeNamePair

---
+++ CodeNamePair.cpp
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include <UtilsStr.h>
+#include <UtilsMisc.h>
+#include <UtilsLog.h>
+
+#include "CodeNamePair.h"
+
+// ---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+// ---------------------------------------------------------------------------
+__fastcall TCodeNamePair::TCodeNamePair() {
+	FCode = CODE_NAME_CODE_NONE;
+	FName = "";
+}
+
+// ---------------------------------------------------------------------------
+__fastcall TCodeNamePair::TCodeNamePair(int Code, String Name) {
+	FCode = Code;
+	FName = Name;
+}
+
+// ---------------------------------------------------------------------------
+__fastcall TCodeNamePair::TCodeNamePair(TCodeNamePair * Value) {
+	Assign(Value);
+}
+
+// ---------------------------------------------------------------------------
+bool __fastcall TCodeNamePair::Equals(TObject * Obj) {
+	if (this == Obj)
+		return true;
+	if (Obj == NULL || ClassType() != Obj->ClassType())
+		return false;
+
+	TCodeNamePair * CodeNamePair = (TCodeNamePair*) Obj;
+
+	if (Code != CodeNamePair->Code || !AnsiSameStr(Name, CodeNamePair->Name)) {
+		return false;
+	}
+
+	return true;
+}
+
+// ---------------------------------------------------------------------------
+void __fastcall TCodeNamePair::Assign(TCodeNamePair * Source) {
+	Code = Source->Code;
+	Name = Source->Name;
+}
+
+// ---------------------------------------------------------------------------
+String __fastcall TCodeNamePair::ToString() {
+	String S;
+
+	S = "TCodeNamePair{";
+	S += "Code='" + IntToStr(Code) + "'";
+	S += ",";
+	S += "Name='" + Name + "'";
+	S += "}";
+
+	return S;
+}
+
+// ---------------------------------------------------------------------------
---
+++ CodeNamePair.h
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------------------------
+
+#ifndef CodeNamePairH
+#define CodeNamePairH
+
+#include <System.Classes.hpp>
+#include <System.SysUtils.hpp>
+
+#include <ObjList.h>
+
+const CODE_NAME_CODE_NONE = 0;
+
+// ---------------------------------------------------------------------------
+class TCodeNamePair : public TObject {
+private:
+	int FCode;
+	String FName;
+
+public:
+	__fastcall TCodeNamePair();
+	__fastcall TCodeNamePair(int Code, String Name);
+	__fastcall TCodeNamePair(TCodeNamePair * Value);
+
+	bool __fastcall Equals(TObject * Obj);
+	void __fastcall Assign(TCodeNamePair * Source);
+	String __fastcall ToString();
+
+	// -----------------------------------------------------------------------
+	__property int Code = {read = FCode, write = FCode};
+	__property String Name = {read = FName, write = FName};
+};
+
+// ---------------------------------------------------------------------------
+typedef TObjList<TCodeNamePair>TCodeNamePairList;
+
+// ---------------------------------------------------------------------------
+#endif
