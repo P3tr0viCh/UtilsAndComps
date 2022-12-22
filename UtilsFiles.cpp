f869680a2d39846c1c082a0bed1556329e5b052f
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2022-12-22 17:04:58 +0500

    UtilsFiles

--- UtilsFiles.cpp
+++ UtilsFiles.cpp
@@ -36,6 +36,7 @@ void CreateWin9xProcessList(TStringList *List) {
 	 end; */
 }
 
+// ---------------------------------------------------------------------------
 void CreateWinNTProcessList(TStringList *List) {
 	DWORD aProcesses[1024];
 	DWORD cbNeeded;
@@ -73,6 +74,7 @@ void CreateWinNTProcessList(TStringList *List) {
 	// List->SaveToFile("Processes.txt");
 }
 
+// ---------------------------------------------------------------------------
 void GetProcessList(TStringList *List) {
 	if (IsWinNT())
 		CreateWinNTProcessList(List);
@@ -80,6 +82,7 @@ void GetProcessList(TStringList *List) {
 		CreateWin9xProcessList(List);
 }
 
+// ---------------------------------------------------------------------------
 bool EXEIsRunning(String FileName, bool FullPath) {
 	String S;
 	TStringList *ProcessList = new TStringList();
@@ -105,6 +108,7 @@ bool EXEIsRunning(String FileName, bool FullPath) {
 	return Result;
 }
 
+// ---------------------------------------------------------------------------
 bool OKExec(const int Inst, String FileName) {
 	bool Result = Inst > 32;
 
@@ -124,20 +128,24 @@ bool OKExec(const int Inst, String FileName) {
 	return Result;
 }
 
+// ---------------------------------------------------------------------------
 bool ShellExec(const String FileName) {
 	return OKExec((int) ShellExecEx(FileName, ""), FileName);
 }
 
+// ---------------------------------------------------------------------------
 HINSTANCE ShellExecEx(const String FileName, const String Params) {
 	return ShellExecute(Application->Handle, NULL, FileName.w_str(),
 		Params.w_str(), NULL, SW_SHOWNORMAL);
 }
 
+// ---------------------------------------------------------------------------
 String FileInAppDir(String FileName) {
 	return IncludeTrailingPathDelimiter(ExtractFilePath(Application->ExeName)) +
 		FileName;
 }
 
+// ---------------------------------------------------------------------------
 String FileWithoutExt(String FileName) {
 	if (Pos('.', FileName) == 0)
 		return FileName;
@@ -151,14 +159,17 @@ String FileWithoutExt(String FileName) {
 	return FileName.SubString(1, PosDot - 1);
 }
 
+// ---------------------------------------------------------------------------
 String OnlyFileName(String FileName) {
 	return FileWithoutExt(ExtractFileName(FileName));
 }
 
+// ---------------------------------------------------------------------------
 String SlashSep(String Path, String S) {
 	return IncludeTrailingPathDelimiter(Path) + S;
 }
 
+// ---------------------------------------------------------------------------
 String GetSpecialFolderPath(int Csidl) {
 	TCHAR pszPath[MAX_PATH];
 
@@ -166,6 +177,34 @@ String GetSpecialFolderPath(int Csidl) {
 		return pszPath;
 	}
 	else {
-		return "";
+		throw EPathNotFoundException("special folder fail: " + SysErrorMessage
+			(GetLastError()));
 	}
 }
+
+// ---------------------------------------------------------------------------
+String GetTempFolderPath() {
+	TCHAR pszPath[MAX_PATH];
+
+	if (GetTempPath(MAX_PATH, pszPath) == 0) {
+		throw EPathNotFoundException("temp folder fail: " + SysErrorMessage
+			(GetLastError()));
+	}
+
+	return pszPath;
+}
+
+// ---------------------------------------------------------------------------
+String GetTempFileNamePath(String Prefix) {
+	TCHAR pszPath[MAX_PATH];
+
+	if (GetTempFileName(GetTempFolderPath().c_str(), Prefix.c_str(), 0,
+		pszPath) == 0) {
+		throw EFileNotFoundException("temp file fail: " + SysErrorMessage
+			(GetLastError()));
+	}
+
+	return pszPath;
+}
+
+// ---------------------------------------------------------------------------
--- UtilsFiles.h
+++ UtilsFiles.h
@@ -11,11 +11,16 @@ bool OKExec(const int Inst, String FileName = NULL);
 bool ShellExec(const String FileName);
 HINSTANCE ShellExecEx(const String FileName, const String Params);
 
+// ---------------------------------------------------------------------------
 String FileInAppDir(String FileName);
 String FileWithoutExt(String FileName);
 String OnlyFileName(String FileName);
 String SlashSep(String Path, String S);
 
+// ---------------------------------------------------------------------------
 String GetSpecialFolderPath(int Csidl);
+String GetTempFolderPath();
+String GetTempFileNamePath(String Prefix);
+
 // ---------------------------------------------------------------------------
 #endif
