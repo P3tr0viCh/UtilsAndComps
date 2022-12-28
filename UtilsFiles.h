b1a52366fed27cf9d0a430a467d1fc31b9d2bc5c
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2022-12-28 15:33:07 +0500

    UtilsFiles
    Удаление лишнего.

--- UtilsFiles.h
+++ UtilsFiles.h
@@ -6,7 +6,6 @@
 #include <System.Types.hpp>
 
 // ---------------------------------------------------------------------------
-bool EXEIsRunning(String FileName, bool FullPath);
 bool OKExec(const int Inst, String FileName = NULL);
 bool ShellExec(const String FileName);
 HINSTANCE ShellExecEx(const String FileName, const String Params);
